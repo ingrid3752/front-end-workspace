ffa290f38377f47b68f75dc2994e39128220f1ca
ingrid3752 <golj6622@naver.com>
2024-10-10 17:54:12 +0900

    [수정] 날씨

---
+++ calendar/src/components/CalendarComponent.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+const CalendarComponent = () => {
+  const [date, setDate] = useState(new Date());
+
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+  };
+
+  return (
+    <div className="calendar-container">
+      <Calendar onChange={handleDateChange} value={date} />
+      <div className="selected-date">
+        <p>선택한 날짜: {date.toLocaleDateString()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarComponent;
